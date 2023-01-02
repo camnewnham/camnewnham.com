76ff34d548cfcd2951dceba6746aed63ac85644d
camnewnham <camnewnham@gmail.com>
2023-01-03 01:42:16 +1100

    remove dynamic loads

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -2,42 +2,13 @@ import * as React from 'react'
 import Head from 'next/head'
 
 import { ExtendedRecordMap } from 'notion-types'
-import { getPageTableOfContents, getPageTitle } from 'notion-utils'
+import { getPageTitle } from 'notion-utils'
 import { NotionRenderer } from 'react-notion-x'
 
-import dynamic from 'next/dynamic'
 import { PageHeader } from './PageHeader'
 
-const Code = dynamic(
-    //@ts-ignore
-    () =>
-        import('react-notion-x/build/third-party/code').then((m) => m.Code)
-)
-const Collection = dynamic(
-    //@ts-ignore
-    () =>
-        import('react-notion-x/build/third-party/collection').then(
-            (m) => m.Collection
-        )
-)
-const Equation = dynamic(
-    //@ts-ignore
-    () =>
-        import('react-notion-x/build/third-party/equation').then((m) => m.Equation)
-)
-const Pdf = dynamic(
-    //@ts-ignore
-    () => import('react-notion-x/build/third-party/pdf').then((m) => m.Pdf),
-    {
-        ssr: false
-    }
-)
-const Modal = dynamic(
-    () => import('react-notion-x/build/third-party/modal').then((m) => m.Modal),
-    {
-        ssr: false
-    }
-)
+import Code from 'react-notion-x/build/third-party/code';
+import Collection from 'react-notion-x/build/third-party/collection';
 
 export const NotionPage = ({
     recordMap,
@@ -69,10 +40,7 @@ export const NotionPage = ({
                 rootPageId={rootPageId}
                 components={{
                     Code,
-                    Collection,
-                    Equation,
-                    Modal,
-                    Pdf
+                    Collection
                 }}
             />
         </>
