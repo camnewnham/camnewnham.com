4b5b9059b0b3e72a84be3da455357ec9b1e6204e
cam <camnewnham@gmail.com>
2023-01-24 16:01:55 +1100

    add next/link and next/image

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -1,48 +1,51 @@
-import * as React from 'react'
-import Head from 'next/head'
+import * as React from "react";
+import Head from "next/head";
 
-import { ExtendedRecordMap } from 'notion-types'
-import { getPageTitle } from 'notion-utils'
-import { NotionRenderer } from 'react-notion-x'
+import { ExtendedRecordMap } from "notion-types";
+import { getPageBreadcrumbs, getPageTitle } from "notion-utils";
+import { NotionRenderer } from "react-notion-x";
 
-import { PageHeader } from './PageHeader'
+import { PageHeader } from "./PageHeader";
 
-import { Code } from 'react-notion-x/build/third-party/code';
-import { Collection } from 'react-notion-x/build/third-party/collection';
+import { Code } from "react-notion-x/build/third-party/code";
+import { Collection } from "react-notion-x/build/third-party/collection";
+import Link from "next/link";
+import Image from "next/image";
 
 export const NotionPage = ({
-    recordMap,
-    rootPageId
+  recordMap,
+  rootPageId,
 }: {
-    recordMap: ExtendedRecordMap
-    rootPageId?: string
+  recordMap: ExtendedRecordMap;
+  rootPageId?: string;
 }) => {
-    if (!recordMap) {
-        return null
-    }
+  if (!recordMap) {
+    return null;
+  }
 
-    const title = getPageTitle(recordMap);
-    console.log(title, recordMap);
+  const title = getPageTitle(recordMap);
+  console.log(title, recordMap);
 
-    return (
-        <>
-            <Head>
-                <meta name='description' content='React Notion X Minimal Demo' />
-                <title>{title} </title>
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>{title} </title>
+      </Head>
 
-            <PageHeader />
+      <PageHeader />
 
-            < NotionRenderer
-                recordMap={recordMap}
-                fullPage={true}
-                darkMode={false}
-                rootPageId={rootPageId}
-                components={{
-                    Code,
-                    Collection
-                }}
-            />
-        </>
-    );
-}
+      <NotionRenderer
+        recordMap={recordMap}
+        fullPage={true}
+        darkMode={false}
+        rootPageId={rootPageId}
+        components={{
+          Code,
+          Collection,
+          nextImage: Image,
+          nextLink: Link,
+        }}
+      />
+    </>
+  );
+};
