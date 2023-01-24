43e548ff01641c076cdbc3535439e99ab74651bb
cam <camnewnham@gmail.com>
2023-01-24 17:02:39 +1100

    add image config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  staticPageGenerationTimeout: 300,
+  images: {
+    domains: [
+      "www.notion.so",
+      "notion.so",
+      "images.unsplash.com",
+      "pbs.twimg.com",
+      "s3.us-west-2.amazonaws.com",
+    ],
+    formats: ["image/avif", "image/webp"],
+  },
+};
