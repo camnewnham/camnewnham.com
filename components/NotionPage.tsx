3ec883aa79320091bd8c93b2d4ab1d0f7f961e16
cam <camnewnham@gmail.com>
2023-01-24 16:25:23 +1100

    test different image link syntax

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -42,8 +42,8 @@ export const NotionPage = ({
         components={{
           Code,
           Collection,
-          nextImage: Image,
-          nextLink: Link,
+          Image,
+          Link,
         }}
       />
     </>
